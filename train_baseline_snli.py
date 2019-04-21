d7e30ab54b65e8284915213b7be1dd82b77b0c4a
Vachel <r06944043@ntu.edu.tw>
2019-04-21 18:19:44 +0800

    snli

--- train_baseline_snli.py
+++ train_baseline_snli.py
@@ -11,21 +11,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.optim as optim
+import torchtext.data as data
+import torchtext.datasets as datasets
+
+from models.mydatasets import datasets
+
 import time
 import numpy as np
 import sys
 from models.baseline_snli import encoder
 from models.baseline_snli import atten
 import argparse
-from models.snli_data import snli_data
-from models.snli_data import w2v
-from random import shuffle
 
+
+# load SST dataset
+def snli(text_field, label_field,  **kargs):
+    train_data, dev_data, test_data = datasets.SNLI.splits(text_field, label_field)
+    # text_field.build_vocab(train_data, dev_data, test_data, vectors=args.w2v_file)
+    text_field.build_vocab(train_data, dev_data, test_data)
+    label_field.build_vocab(train_data, dev_data, test_data)
+    train_iter, dev_iter, test_iter = data.BucketIterator.splits(
+                                        (train_data, dev_data, test_data), 
+                                        batch_sizes=(args.batch_size, 
+                                                     args.batch_size, 
+                                                     args.batch_size),
+                                        **kargs)
+    return train_iter, dev_iter, test_iter 
 
 def train(args):
-
-    if args.max_length < 0:
-        args.max_length = 9999
 
     # initialize the logger
     # create logger
@@ -43,35 +56,33 @@ def train(args):
     console.setLevel(logging.INFO)
     logger.addHandler(console)
 
-    torch.cuda.set_device(args.gpu_id)
-
     for arg in vars(args):
         logger.info(str(arg) + ' ' + str(getattr(args, arg)))
 
     # load train/dev/test data
     # train data
     logger.info('loading data...')
-    train_data = snli_data(args.train_file, args.max_length)
-    train_batches = train_data.batches
-    train_lbl_size = 3
-    dev_data = snli_data(args.dev_file, args.max_length)
-    dev_batches = dev_data.batches
-    test_data = snli_data(args.test_file, args.max_length)
-    test_batches = test_data.batches
-    logger.info('train size # sent ' + str(train_data.size))
-    logger.info('dev size # sent ' + str(dev_data.size))
-    logger.info('test size # sent ' + str(test_data.size))
+    text_field = data.Field(lower=True, batch_first = True)
+    label_field = data.Field(sequential=False)
+    train_iter, dev_iter, test_iter = snli(text_field, label_field, repeat=False)
+
+    batch_num_train = int(len(train_iter.dataset) / args.batch_size)
+    train_lbl_size = len(label_field.vocab) - 1
+    
+    logger.info('train size # sent ' + str(len(train_iter.dataset)))
+    logger.info('dev size # sent ' + str(len(dev_iter.dataset)))
+    logger.info('test size # sent ' + str(len(test_iter.dataset)))
 
     # get input embeddings
     logger.info('loading input embeddings...')
-    word_vecs = w2v(args.w2v_file).word_vecs 
+    vocab = text_field.vocab
 
     best_dev = []   # (epoch, dev_acc)
 
     # build the model
-    input_encoder = encoder(word_vecs.size(0), args.embedding_size, args.hidden_size, args.para_init)
-    input_encoder.embedding.weight.data.copy_(word_vecs)
-    input_encoder.embedding.weight.requires_grad = False
+    input_encoder = encoder(len(vocab), args.embedding_size, args.hidden_size, args.para_init)
+    # input_encoder.embedding.weight.data.copy_(vocab.vectors)
+    # input_encoder.embedding.weight.requires_grad = False
     inter_atten = atten(args.hidden_size, train_lbl_size, args.para_init)
 
     input_encoder.cuda()
@@ -101,18 +112,15 @@ def train(args):
         loss_data = 0.
         train_sents = 0.
 
-        shuffle(train_batches)
         timer = time.time()
-        for i in range(len(train_batches)):
+        for i, batch in enumerate(train_iter):
+            train_src_batch, train_tgt_batch, train_lbl_batch = batch.premise, batch.hypothesis, batch.label
+            train_lbl_batch.sub_(1)
 
-            train_src_batch, train_tgt_batch, train_lbl_batch = train_batches[i]
+            if args.cuda:
+                train_src_batch, train_tgt_batch, train_lbl_batch = train_src_batch.cuda(), train_tgt_batch.cuda(), train_lbl_batch.cuda()
 
-            train_src_batch = Variable(train_src_batch.cuda())
-            train_tgt_batch = Variable(train_tgt_batch.cuda())
-            train_lbl_batch = Variable(train_lbl_batch.cuda())
-
-            batch_size = train_src_batch.size(0)
-            train_sents += batch_size
+            train_sents += args.batch_size
 
             input_optimizer.zero_grad()
             inter_atten_optimizer.zero_grad()
@@ -143,7 +151,7 @@ def train(args):
                 if isinstance(m, nn.Linear):
                     grad_norm += m.weight.grad.data.norm() ** 2
                     para_norm += m.weight.data.norm() ** 2
-                    if m.bias:
+                    if m.bias is not None:
                         grad_norm += m.bias.grad.data.norm() ** 2
                         para_norm += m.bias.data.norm() ** 2
 
@@ -151,12 +159,12 @@ def train(args):
                 if isinstance(m, nn.Linear):
                     grad_norm += m.weight.grad.data.norm() ** 2
                     para_norm += m.weight.data.norm() ** 2
-                    if m.bias:
+                    if m.bias is not None:
                         grad_norm += m.bias.grad.data.norm() ** 2
                         para_norm += m.bias.data.norm() ** 2
 
-#             grad_norm ** 0.5
-#             para_norm ** 0.5
+            grad_norm ** 0.5
+            para_norm ** 0.5
 
             shrinkage = args.max_grad_norm / grad_norm
             if shrinkage < 1 :
@@ -175,21 +183,23 @@ def train(args):
 
             _, predict = log_prob.data.max(dim=1)
             total += train_lbl_batch.data.size()[0]
+
             correct += torch.sum(predict == train_lbl_batch.data)
-            loss_data += (loss.data[0] * batch_size)  # / train_lbl_batch.data.size()[0])
+
+            loss_data += (loss.data * args.batch_size)  # / train_lbl_batch.data.size()[0])
 
             if (i + 1) % args.display_interval == 0:
                 logger.info('epoch %d, batches %d|%d, train-acc %.3f, loss %.3f, para-norm %.3f, grad-norm %.3f, time %.2fs, ' %
-                            (k, i + 1, len(train_batches), correct / total,
+                            (k, i + 1, batch_num_train, correct.cpu().data.numpy() / total,
                              loss_data / train_sents, para_norm, grad_norm, time.time() - timer))
                 train_sents = 0.
                 timer = time.time()
                 loss_data = 0.
                 correct = 0.
                 total = 0.
-            if i == len(train_batches) - 1:
+            if i == batch_num_train - 1:
                 logger.info('epoch %d, batches %d|%d, train-acc %.3f, loss %.3f, para-norm %.3f, grad-norm %.3f, time %.2fs, ' %
-                            (k, i + 1, len(train_batches), correct / total,
+                            (k, i + 1, batch_num_train, correct.cpu().data.numpy() / total,
                              loss_data / train_sents, para_norm, grad_norm, time.time() - timer))
                 train_sents = 0.
                 timer = time.time()
@@ -203,12 +213,12 @@ def train(args):
             inter_atten.eval()
             correct = 0.
             total = 0.
-            for i in range(len(dev_batches)):
-                dev_src_batch, dev_tgt_batch, dev_lbl_batch = dev_batches[i]
+            for batch in dev_iter:
+                dev_src_batch, dev_tgt_batch, dev_lbl_batch = batch.premise, batch.hypothesis, batch.label
+                dev_lbl_batch.sub_(1)
 
-                dev_src_batch = Variable(dev_src_batch.cuda())
-                dev_tgt_batch = Variable(dev_tgt_batch.cuda())
-                dev_lbl_batch = Variable(dev_lbl_batch.cuda())
+                if args.cuda:
+                    dev_src_batch, dev_tgt_batch, dev_lbl_batch = dev_src_batch.cuda(), dev_tgt_batch.cuda(), dev_lbl_batch.cuda()
 
                 # if dev_lbl_batch.data.size(0) == 1:
                 #     # simple sample batch
@@ -223,7 +233,7 @@ def train(args):
                 total += dev_lbl_batch.data.size()[0]
                 correct += torch.sum(predict == dev_lbl_batch.data)
 
-            dev_acc = correct / total
+            dev_acc = correct.cpu().data.numpy() / total
             logger.info('dev-acc %.3f' % (dev_acc))
 
             if (k + 1) / args.dev_interval == 1:
@@ -261,12 +271,12 @@ def train(args):
     correct = 0.
     total = 0.
 
-    for i in range(len(test_batches)):
-        test_src_batch, test_tgt_batch, test_lbl_batch = test_batches[i]
+    for batch in test_iter:
+        test_src_batch, test_tgt_batch, test_lbl_batch = batch.premise, batch.hypothesis, batch.label
+        test_lbl_batch.sub_(1)
 
-        test_src_batch = Variable(test_src_batch.cuda())
-        test_tgt_batch = Variable(test_tgt_batch.cuda())
-        test_lbl_batch = Variable(test_lbl_batch.cuda())
+        if args.cuda:
+            test_src_batch, test_tgt_batch, test_lbl_batch = test_src_batch.cuda(), test_tgt_batch.cuda(), test_lbl_batch.cuda()
 
         test_src_linear, test_tgt_linear=input_encoder(
             test_src_batch, test_tgt_batch)
@@ -276,7 +286,7 @@ def train(args):
         total += test_lbl_batch.data.size()[0]
         correct += torch.sum(predict == test_lbl_batch.data)
 
-    test_acc = correct / total
+    test_acc = correct.cpu().data.numpy() / total
     logger.info('test-acc %.3f' % (test_acc)) 
 
 
@@ -284,30 +294,23 @@ if __name__ == '__main__':
     parser=argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('--train_file', help='training data file (hdf5)',
-                        type=str, default='/disk/scratch/bowenli/nmt/struct-attn/data/snli/baseline/entail-train.hdf5')
-
-    parser.add_argument('--dev_file', help='development data file (hdf5)',
-                        type=str, default='/disk/scratch/bowenli/nmt/struct-attn/data/snli/baseline/entail-val.hdf5')
-
-    parser.add_argument('--test_file', help='test data file (hdf5)',
-                        type=str, default='/disk/scratch/bowenli/nmt/struct-attn/data/snli/baseline/entail-test.hdf5')
-
-    parser.add_argument('--w2v_file', help='pretrained word vectors file (hdf5)',
-                        type=str, default='/disk/scratch/bowenli/nmt/struct-attn/data/snli/baseline/glove.hdf5')
+    parser.add_argument('--w2v_file', help='pretrained word vectors file',
+                        type=str, default='glove.6B.200d')
 
     parser.add_argument('--log_dir', help='log file directory',
-                        type=str, default='/disk/scratch/bowenli/nmt/struct-attn/data/snli/experiment_struc/')
+                        type=str, default='log_dir')
 
     parser.add_argument('--log_fname', help='log file name',
                         type=str, default='log54.log')
 
     parser.add_argument('--gpu_id', help='GPU device id',
-                        type=int, default=1)
+                        type=int, default=0)
+
+    parser.add_argument('--batch_size', help='batch size',
+                        type=int, default=64)
 
     parser.add_argument('--embedding_size', help='word embedding size',
-                        type=int, default=300)
+                        type=int, default=100)
 
     parser.add_argument('--epoch', help='training epoch',
                         type=int, default=250)
@@ -325,11 +328,11 @@ if __name__ == '__main__':
                         type=float, default=0.05)
 
     parser.add_argument('--hidden_size', help='hidden layer size',
-                        type=int, default=300)
+                        type=int, default=100)
 
     parser.add_argument('--max_length', help='maximum length of training sentences,\
                         -1 means no length limit',
-                        type=int, default=10)
+                        type=int, default=20)
 
     parser.add_argument('--display_interval', help='interval of display',
                         type=int, default=1000)
@@ -345,9 +348,16 @@ if __name__ == '__main__':
                         type=float, default=5e-5)
 
     parser.add_argument('--model_path', help='path of model file (not include the name suffix',
-                        type=str, default='/disk/scratch/bowenli/nmt/struct-attn/data/snli/experiment_struc/')
+                        type=str, default='snapshots/')
+
+    parser.add_argument('--cuda', action='store_true', default=True, help='gpu')
 
     args=parser.parse_args()
+
+    args.cuda = (args.cuda) and torch.cuda.is_available()
+    if args.cuda:
+        torch.cuda.set_device(args.gpu_id)
+
     # args.max_lenght = 10   # args can be set manually like this
     train(args)
 
